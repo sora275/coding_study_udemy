05aca35c3751bc751fe91cc36438b7c1b4b79b30
Sora.K <zjvmf34@gmail.com>
2022-05-14 00:59:10 +0900

    220514 coding

--- 07_time/app.js
+++ 07_time/app.js
@@ -1,5 +1,34 @@
 let age = 32;
 let userName = 'Max';
-let hobbies = ['Sports', 'Cooking', 'Reading'];
+let hobbies = [
+    'Sports',
+    'Cooking',
+    'Reading'
+];
 
-alert(hobbies[2]);
+let job = {
+    title: 'Developer', 
+    place: 'New York', 
+    salary: 50000
+};
+
+let adultYears;
+
+function calculateAdultYears(userAge) {
+    return userAge - 18;
+};
+
+adultYears = calculateAdultYears(age);
+alert(adultYears);
+
+age = 45;
+
+adultYears = calculateAdultYears(age);
+alert(adultYears);
+
+
+
+
+
+// alert(hobbies[2]);
+// alert(job.title)
